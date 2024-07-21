42a0c861fb70d56d71b70f61ab412e7a91926b1e
ajaka <ajaka@DESKTOP-BOKNB4N>
2024-07-21 16:42:20 +0530

    b.committed

--- Proj1/src/main/java/com/aj/App.java
+++ Proj1/src/main/java/com/aj/App.java
@@ -1,10 +1,26 @@
 package com.aj;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       Scanner sc=new Scanner(System.in);
+       System.out.println("enter a number:");
+       int num=sc.nextInt();
+       int count=0;
+       for(int i=2;i<num/2;i++) {
+    	   if(num%i==0) {
+    		  count++; 
+    	   }
+       }
+       
+       if(count==0) {
+    	   System.out.println("it is prime number");
+       }else {
+    	   System.out.println("it is not a prime number");
+       }
         
     }
 }
